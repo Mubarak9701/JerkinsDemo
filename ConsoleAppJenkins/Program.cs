2a3e393e2c1680da6d841a7cb82772aa089cb372
Mubarak Lepakshi <Mubarak_Lepakshi@epam.com>
2022-08-24 14:19:18 +0530

    updated

--- ConsoleAppJenkins/Program.cs
+++ ConsoleAppJenkins/Program.cs
@@ -4,8 +4,10 @@ namespace ConsoleAppJenkins
 {
     public class Program
     {
+        string s;
         public static void Main()
         {
+            int a, b;
             Console.WriteLine("Hello World!");
         }
     }
